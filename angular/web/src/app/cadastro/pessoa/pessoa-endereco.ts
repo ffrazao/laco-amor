bf8d77840aeebe36be84190e873ee90ca3464794
Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>
2020-04-13 18:19:40 -0300

    Estrutura
    Modificação da estrutura Web para um subrepositório. A pasta local foi apagada
    
    Signed-off-by: Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>

--- angular/web/src/app/cadastro/pessoa/pessoa-endereco.ts
+++
@@ -1,8 +0,0 @@
-import { Pessoa } from './pessoa'
-import { Endereco } from '../endereco/endereco';
-
-export interface PessoaEndereco {
-    id: number,
-    pessoa: Pessoa,
-    endereco: Endereco
-}
--- angular/web/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'http://localhost:8080',
-  OAUTH_URL: 'http://localhost:8081',
-};
