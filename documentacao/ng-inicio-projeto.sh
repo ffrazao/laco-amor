c29283bdc2371410eaaacba18d6a5ca65c6fd442
Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>
2020-04-25 22:40:39 -0300

    Ajustes
    Script de iniciação do projeto
    Renomear o modelo
    
    Signed-off-by: Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>

---
+++ documentacao/ng-inicio-projeto.sh
@@ -0,0 +1,70 @@
+ng g m acao --routing=true
+
+ng g m acao/cotar --routing=true
+ng g c acao/cotar --skip-tests
+ng g s acao/cotar/cotar --skip-tests
+ng g class acao/cotar/cotar --skip-tests
+ng g class acao/cotar/cotar-filtro --skip-tests
+
+ng g m acao/cotar/filtro --routing=true
+ng g c acao/cotar/filtro --skip-tests
+
+ng g m acao/cotar/form --routing=true
+ng g c acao/cotar/form --skip-tests
+
+ng g m acao/cotar/list --routing=true
+ng g c acao/cotar/list --skip-tests
+
+ng g m acao/comprar --routing=true
+ng g c acao/comprar --skip-tests
+ng g s acao/comprar/comprar --skip-tests
+ng g class acao/comprar/comprar --skip-tests
+ng g class acao/comprar/comprar-filtro --skip-tests
+
+ng g m acao/comprar/filtro --routing=true
+ng g c acao/comprar/filtro --skip-tests
+
+ng g m acao/comprar/form --routing=true
+ng g c acao/comprar/form --skip-tests
+
+ng g m acao/comprar/list --routing=true
+ng g c acao/comprar/list --skip-tests
+
+ng g m acao/produzir --routing=true
+ng g c acao/produzir --skip-tests
+ng g s acao/produzir/produzir --skip-tests
+ng g class acao/produzir/produzir --skip-tests
+ng g class acao/produzir/produzir-filtro --skip-tests
+
+ng g m acao/produzir/filtro --routing=true
+ng g c acao/produzir/filtro --skip-tests
+
+ng g m acao/produzir/form --routing=true
+ng g c acao/produzir/form --skip-tests
+
+ng g m acao/produzir/list --routing=true
+ng g c acao/produzir/list --skip-tests
+
+ng g m acao/vender --routing=true
+ng g c acao/vender --skip-tests
+ng g s acao/vender/vender --skip-tests
+ng g class acao/vender/vender --skip-tests
+ng g class acao/vender/vender-filtro --skip-tests
+
+ng g m acao/vender/filtro --routing=true
+ng g c acao/vender/filtro --skip-tests
+
+ng g m acao/vender/form --routing=true
+ng g c acao/vender/form --skip-tests
+
+ng g m acao/vender/list --routing=true
+ng g c acao/vender/list --skip-tests
+
+ng g m cadastro --routing=true
+
+ng g m cadastro/pessoa --routing=true
+ng g c cadastro/pessoa --skip-tests
+
+ng g m cadastro/produto-modelo --routing=true
+ng g c cadastro/produto-modelo --skip-tests
+
